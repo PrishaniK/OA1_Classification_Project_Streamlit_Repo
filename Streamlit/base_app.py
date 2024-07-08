6f515f188387e1d279816f620417cfabbd664591
PrishaniK <prishanik135@gmail>
2024-07-08 15:26:46 +0200

    update base_app file

--- Streamlit/base_app.py
+++ Streamlit/base_app.py
@@ -21,28 +21,18 @@
 	https://docs.streamlit.io/en/latest/
 
 """
-# Streamlit dependencies
+# # Streamlit dependencies
 import streamlit as st
-import joblib,os
-
-# Data dependencies
-import pandas as pd
-
-import streamlit as st
-from textblob import TextBlob
-from sklearn.feature_extraction.text import CountVectorizer
 import joblib
-import pandas as pd
 import os
+import pandas as pd
+from PIL import Image
 import re
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Define path to the directory containing all pickled files
 MODEL_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -53,99 +43,33 @@ svm_path = os.path.join(MODEL_DIR, 'svm_model.pkl')
 naive_bayes_path = os.path.join(MODEL_DIR, 'naive_bayes_model.pkl')
 tfidf_vectorizer_path = os.path.join(MODEL_DIR, 'tfidf_vectorizer.pkl')
 
-# Define text cleaning function
-def clean_text(text, is_url=False):
-    if isinstance(text, str):
-        # Handle the URL in the text
-        if is_url:
-            text = re.sub(r'https?://[^/]+/', '', text)
-            text = re.sub(r'[^a-zA-Z0-9]', ' ', text) 
-        else:
-            # Lowercase the text
-            text = text.lower()
-            
-            # Remove punctuation using string
-            text = text.translate(str.maketrans('', '', string.punctuation))
+# Load vectorizer
+with open(tfidf_vectorizer_path, 'rb') as file:
+    tfidf_vectorizer = joblib.load(file)
 
-            # Remove numerical values in the text
-            text = re.sub(r'\d+', '', text)
-        
-            # Tokenize the text
-            tokens = word_tokenize(text)
-        
-            # Remove stop words
-            stop_words = set(stopwords.words('english'))
-            tokens = [word for word in tokens if word not in stop_words]
-        
-            # Lemmatization
-            lemmatizer = WordNetLemmatizer()
-            tokens = [lemmatizer.lemmatize(word) for word in tokens]
-        
-            # Remove special characters and extra whitespeaces
-            tokens = [re.sub(r'\W+', '', word) for word in tokens]
-            
-            # Filter out empty strings and single letter words
-            tokens = [word for word in tokens if word and len(word) > 1]
-            
-            # Ensure that words are written as individual words
-            distinct_tokens = set(tokens)
-        
-            # Join the tokens back together
-            text = ' '.join(distinct_tokens)
-        
-        return text 
-    else:  
-        return text
-    
-def load_model(file_path):
-    with open(file_path, 'rb') as file:
-        model = joblib.load(file)
-    return model
+# Load models
+models = {
+    "Logistic Regression": joblib.load(logistic_regression_path),
+    "Support Vector Machine": joblib.load(svm_path),
+    "Naive Bayes": joblib.load(naive_bayes_path)
+}
 
-
+# Load data
+train_data_path = os.path.join(MODEL_DIR, 'train.csv')
+df = pd.read_csv(train_data_path)
 
 def main():
-    """News classifier"""
-
-    # Load vectorizer
-    with open(tfidf_vectorizer_path, 'rb') as file:
-        tfidf_vectorizer = joblib.load(file)
-    
-    # Define available models
-    models = {
-        "Logistic Regression": logistic_regression_path,
-        "Support Vector Machine": svm_path,
-        "Naive Bayes": naive_bayes_path
-    }
-	
-	# Define path to the training dataset
-    train_data_path = "train.csv"
-
-    # Load your raw data
-    df = pd.read_csv(train_data_path)
-    # Creates a main title and subheader on your page -
-    # these are static across all pages
     st.title("News Feast")
-    st.subheader("Analysing news articles")
+    st.subheader("Analyzing news articles")
 
-    # Creating sidebar with selection box -
-    # you can create multiple pages this way
-    options = ["Prediction", "Information"]
+    # Displaying the picture under the title and subheader
+    st.image(Image.open(os.path.join(MODEL_DIR, 'vintage-newspaper.jpg')), use_column_width=True)
 
-    # Ensure each widget has a unique key to avoid DuplicateWidgetID error
+    options = ["Prediction", "Information", "Feedback", "About Us"]
     selection = st.sidebar.selectbox("Choose Option", options)
 
-    # Model selection dropdown
-    selected_model = st.sidebar.selectbox("Choose Model", list(models.keys()))
-
-    # Load selected model
-    model_path = models[selected_model]
-    model = load_model(model_path)
-
-    # Building out the "Information" page
     if selection == "Information":
         st.info("General Information")
-        # You can read a markdown file from supporting resources folder
         st.markdown("""
             "Explore the world of news with our innovative app! From Business to Technology, Sports to Entertainment, and beyond, discover insights from a diverse collection of articles. Our app uses advanced machine learning to predict categories for new articles, ensuring you stay informed effortlessly. Experience seamless interaction and practical utility, making knowledge accessible anytime, anywhere. Dive into the future of news analysis with us!"
             This application demonstrates a news classifier using different machine learning models. (Currently only trained using Business, Entertainment, Technology, Education and Sport categories.)
@@ -162,22 +86,52 @@ def main():
             For more information on Streamlit, refer to the [Streamlit Documentation](https://docs.streamlit.io/en/latest/).
         """)
 
-        
-    # Building out the prediction page
-    if selection == "Prediction":
-        st.info("Prediction with {} Model".format(selected_model))
+    elif selection == "Prediction":
+        st.info("Prediction")
+        selected_model = st.selectbox("Choose Model", list(models.keys()))
+
         # Creating a text box for user input
         news_text = st.text_area("Enter Text", "Type Here")
 
         if st.button("Classify"):
             # Transforming user input with vectorizer
             vect_text = tfidf_vectorizer.transform([news_text]).toarray()
+
             # Make predictions using selected model
+            model = models[selected_model]
             prediction = model.predict(vect_text)
-            
+
             # When model has successfully run, will print prediction
             st.success("Text Categorized as: {}".format(prediction[0]))
 
-# Required to let Streamlit instantiate our web app.  
+    elif selection == "Feedback":
+        st.info("Feedback")
+        st.markdown("""
+            Your feedback is valuable to us! Please provide your comments and suggestions in the text area to help us improve this application.
+            """
+        )
+        feedback_text = st.text_area("Your Feedback", "")
+        if st.button("Submit Feedback"):
+            st.success("Thank you for your feedback!")
+
+    elif selection == "About Us":
+        st.info("About Us")
+        st.markdown("""
+
+            For any inquiries or further information, please contact any one from our time: 
+
+            ---
+            **Our Team:**
+            - Koketso Bambo	    moraka1952@gmail.com
+            - Thobile Mvuni	    thoyomvuni@gmail.com
+            - Prishani Kisten	    prishanik135@gmail.com
+            - Chuma Gqola	        chuma.gqolacg@gmail.com
+            - Kgolo Motshegoa	    motshegoakgolo@gmail.com
+            - Lulama Mulaudzi	    lulamamulaudzi@gmail.com
+
+            **Vision:**
+            "To revolutionize information access by seamlessly categorizing news articles, empowering users with accurate and insightful content classification."
+        """)
+
 if __name__ == '__main__':
-	main()
+    main()
